394c43c945547f033499519d08d5350ae26c5a2f
Remi Gau <remi_gau@hotmail.com>
2020-09-20 21:15:32 +0200

    Initial commit

---
+++ ui-changes/src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  /* eslint-disable */
+  githubSrc: 'https://raw.githubusercontent.com/user_name/repo_name/branch_name/protocols/schema_file_name.jsonld',
+  banner: 'This protocol is under construction.',
+  startButton: 'Start',
+  assetsPublicPath: '/reproschema_template/',
+  backendServer: 'null'
+}
