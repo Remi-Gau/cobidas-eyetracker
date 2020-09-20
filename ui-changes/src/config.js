877856b967941dc864a5ffa92267fdf31a80b3f9
Remi Gau <remi_gau@hotmail.com>
2020-09-20 23:30:02 +0200

    fix public path

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas-eyetracker/master/protocols/cobidas_eyetracker/cobidas_eyetracker_schema',
   banner: 'This protocol is under construction.',
   startButton: 'Start',
-  assetsPublicPath: '/reproschema_template/',
+  assetsPublicPath: '/cobidas_eyetracker/',
   backendServer: 'null'
 }
 
