932548899b7353275f77d2499441e0d1c4e4fcf4
Remi Gau <remi_gau@hotmail.com>
2020-09-20 21:27:26 +0200

    add protocol and activity

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,10 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/user_name/repo_name/branch_name/protocols/schema_file_name.jsonld',
+  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas-eyetracker/master/protocols/cobidas_eyetracker/cobidas_eyetracker_schema',
   banner: 'This protocol is under construction.',
   startButton: 'Start',
   assetsPublicPath: '/reproschema_template/',
   backendServer: 'null'
 }
+
+
