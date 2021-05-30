4e70ca9bc82b9b34c48ef97a309a3278940fc0a2
Remi Gau <remi_gau@hotmail.com>
2021-05-30 22:52:10 +0200

    transfer jsonld to eCOBIDAS repo

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,10 +1,9 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas-eyetracker/master/protocols/eyetracker/eyetracker_schema',
-  banner: 'This protocol is under construction.',
-  startButton: 'Start',
-  assetsPublicPath: '/cobidas-eyetracker/',
-  backendServer: 'null'
-}
-
-
+  githubSrc:
+    "https://raw.githubusercontent.com/ohbm/eCOBIDAS/master/protocols/eyetracker/eyetracker_schema",
+  banner: "This protocol is under construction.",
+  startButton: "Start",
+  assetsPublicPath: "/cobidas-eyetracker/",
+  backendServer: "null",
+};
