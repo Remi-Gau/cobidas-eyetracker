11c973b186a2c291b23b100058f535b70761e89c
Remi Gau <remi_gau@hotmail.com>
2021-06-26 16:38:30 +0200

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc:
-    "https://raw.githubusercontent.com/ohbm/eCOBIDAS/master/schemas/eyetracking/protocols/eyetracking_schema.jsonld",
+    "https://raw.githubusercontent.com/ohbm/cobidas_schema/master/schemas/eyetracking/protocols/eyetracking_schema.jsonld",
   banner: "This protocol is under construction.",
   startButton: "Start",
   assetsPublicPath: "/cobidas-eyetracker/",
